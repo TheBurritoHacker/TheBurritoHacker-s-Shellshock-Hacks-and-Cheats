e1206f0c4d5ad3e6da21c54263423377c7e6544c
TheBurritoHacker <79960279+TheBurritoHacker@users.noreply.github.com>
2021-03-15 20:45:09 -0700

    Create BurritoHacker=Userscript.js

---
+++ BurritoHacker=Userscript.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         TheBurritoHacker
+// @version      0.1
+// @description  A quirky american Shell Shockers gamer/hacker
+// @author       ******* Li
+// @match        Github.com/TheBurritoHacker
+// @grant        none
+// ==/UserScript==
